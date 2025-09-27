2e79b02ec08616454e9d480a42463729b6b64d3b
Ajaya Ramachandran <ramachandran.aj@northeastern.edu>
2025-09-27 01:13:39 -0400

    changed navbar vert

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ function Navbar() {
   return (
     <>
       <div className='navbar'>
-        <a href='https://www.nimbial.com' style={{color: '#dd7c05ff', fontSize: '14px'}}><b>nimbial.com</b></a>
-        <a href='https://symphony.nimbial.com'>Symphony</a>
-        <a className='gray'>Tasklist</a>
-        <a className='gray'>Stratus</a>
-        <a className='gray'>Powerflows</a>
+        <a className='item' href='https://www.nimbial.com' style={{color: '#dd7c05ff', fontSize: '14px'}}><b>nimbial.com</b></a>
+        <a className='item' href='https://symphony.nimbial.com'>Symphony</a>
+        <a className='item gray'>Tasklist</a>
+        <a className='item gray'>Stratus</a>
+        <a className='item gray'>Powerflows</a>
       </div>
     </>
   );
