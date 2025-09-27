6cb6d3f62714466e16a9c9eab018c1a1093d562c
Ajaya Ramachandran <ramachandran.aj@northeastern.edu>
2025-09-27 01:09:23 -0400

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <>
       <div className='navbar'>
-        <a href='https://www.nimbial.com'><b>nimbial.com</b></a>
+        <a href='https://www.nimbial.com' style={{color: '#dd7c05ff', fontSize: '14px'}}><b>nimbial.com</b></a>
         <a href='https://symphony.nimbial.com'>Symphony</a>
         <a className='gray'>Tasklist</a>
         <a className='gray'>Stratus</a>
