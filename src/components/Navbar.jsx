54895231dd4f7aef66a1f47f8b28842a4591283e
Ajaya Ramachandran <ramachandran.aj@northeastern.edu>
2025-12-08 20:09:37 -0500

    added more navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,8 @@ function Navbar() {
       <div className='navbar'>
         <a className='item' href='https://www.nimbial.com' style={{color: '#dd7c05ff', fontSize: '14px'}}><b>nimbial.com</b></a>
         <a className='item' href='https://symphony.nimbial.com'>Symphony</a>
+        <a className='item' href='https://docs.nimbial.com'>Docs</a>
+        <a className='item' href='https://blog.nimbial.com'>Blog</a>
         <a className='item gray'>Tasklist</a>
         <a className='item gray'>Stratus</a>
         <a className='item gray'>Powerflows</a>
